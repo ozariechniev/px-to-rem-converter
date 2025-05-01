28da82625db98f8102e1e3e3a5ef0f93ff9925a9
Oleksandr Zariechniev <158515671+ozariechniev@users.noreply.github.com>
2025-05-01 22:58:27 +0200

    Enhance layout metadata by updating the title, description, and adding authors, creator, and publisher information. Introduce Open Graph metadata for improved social sharing and SEO, along with additional robots directives for better indexing.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,15 +20,43 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: {
-    default: 'PX TO REM',
+    default: 'PX TO REM Converter',
     template: '%s | PX TO REM Converter',
   },
   description:
-    'Convert PX to REM with ease. A free online tool for web developers to quickly convert pixel values to rem units for responsive web design.',
+    'Free online PX to REM converter tool for web developers. Instantly convert pixel values to rem units for responsive and accessible web design. Perfect for CSS developers and designers.',
   keywords: ['px to rem', 'pixel converter', 'rem converter', 'css units', 'responsive design', 'web development'],
+  authors: [{ name: 'Oleksandr Zariechniev' }],
+  creator: 'Oleksandr Zariechniev',
+  publisher: 'Oleksandr Zariechniev',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  metadataBase: new URL(process.env.NEXT_PUBLIC_URL || ''),
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    title: 'PX TO REM Converter',
+    description:
+      'Free online PX to REM converter tool for web developers. Instantly convert pixel values to rem units for responsive and accessible web design. Perfect for CSS developers and designers.',
+    url: process.env.NEXT_PUBLIC_URL || '',
+    siteName: 'PX TO REM',
+    locale: 'en_US',
+    type: 'website',
+  },
   robots: {
     index: true,
     follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
   },
 };
 
